94816317de615a281b0cc002e8ae433fa46e37f5
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2024-03-13 19:23:07 +0100

    autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,14 +24,30 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
             local lspcfg = require("lspconfig")
-            lspcfg.lua_ls.setup({})
-            lspcfg.clangd.setup({})
-            lspcfg.cmake.setup({})
-            lspcfg.omnisharp.setup({})
-            lspcfg.pylsp.setup({})
-            lspcfg.rust_analyzer.setup({})
-            lspcfg.zls.setup({})
+            lspcfg.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspcfg.clangd.setup({
+                capabilities = capabilities
+            })
+            lspcfg.cmake.setup({
+                capabilities = capabilities
+            })
+            lspcfg.omnisharp.setup({
+                capabilities = capabilities
+            })
+            lspcfg.pylsp.setup({
+                capabilities = capabilities
+            })
+            lspcfg.rust_analyzer.setup({
+                capabilities = capabilities
+            })
+            lspcfg.zls.setup({
+                capabilities = capabilities
+            })
 
             -- setup other lsp's
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
