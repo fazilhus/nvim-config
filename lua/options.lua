e29f324bb0de211e72e3ba341d98a1796e3d9d1e
Fazil Khusnutdinov <khusnutdinov.fazil@gmail.com>
2024-03-13 07:54:45 +0100

    init

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+opt.number = true
+
+opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = false
+opt.background = "dark"
+
+vim.g.mapleader = " "
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return
+{
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd", "omnisharp", "neocmake", "pylsp", "rust_analyzer", "zls" },
+            })
+        end
+    },
+    {
+       "neovim/nvim-lspconfig",
+        config = function()
+            local lspcfg = require("lspconfig")
+            lspcfg.lua_ls.setup({})
+            -- setup other lsp's
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gD', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
